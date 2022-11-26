cd364de9a60885c50645ca5b3dc6481ad262554e
dreazz97 <iuripeniche@hotmail.com>
2022-11-26 22:56:47 +0000

    Portfolio Pessoal

---
+++ index.js
@@ -0,0 +1,8 @@
+var github_link = document.querySelector(".github");
+github_link.addEventListener("click", function() {
+    location.href="https://github.com/dreazz97";
+})
+var linkedin_link = document.querySelector(".linkedin");
+linkedin_link.addEventListener("click", function() {
+    location.href="https://pt.linkedin.com/in/iuri-peniche-29337a183";
+})
